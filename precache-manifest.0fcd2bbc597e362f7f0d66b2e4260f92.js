49069d199a222d7e8c07258ee51bae8f75dc2aef
Vasanthan <vasanth2754@gmail.com>
2020-10-20 21:07:35 +0530

    Updates

---
+++ precache-manifest.0fcd2bbc597e362f7f0d66b2e4260f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72df861976003b808ae72de5d0e58200",
+    "url": "/react-slider/index.html"
+  },
+  {
+    "revision": "f973013d9fa8ecf6dd53",
+    "url": "/react-slider/static/css/main.32455f06.chunk.css"
+  },
+  {
+    "revision": "57304ffb6d027473a70b",
+    "url": "/react-slider/static/js/2.64f42d7a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-slider/static/js/2.64f42d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f973013d9fa8ecf6dd53",
+    "url": "/react-slider/static/js/main.e7a050e4.chunk.js"
+  },
+  {
+    "revision": "14182e5e6d736833f2dc",
+    "url": "/react-slider/static/js/runtime-main.4b11de5c.js"
+  }
+]);
